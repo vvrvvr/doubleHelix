34fb76e49f20ca1305e4163eb2f37712dd9184a0
v.svistunov <v.svistunov@corp.nekki.ru>
2023-10-12 22:37:02 +0300

    impulse generation in damage zone

--- double_test/Assets/Scripts/DamagePlayerZone.cs
+++ double_test/Assets/Scripts/DamagePlayerZone.cs
@@ -1,13 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class DamagePlayerZone : MonoBehaviour, IInteractable
 {
+    private CinemachineImpulseSource impulseSource;
     public InteractionOption InteractionOption { get; set; }
     public void Interact()
     {
         GameManager.Instance.ReduceLives();
+        impulseSource = GetComponent<CinemachineImpulseSource>();
+        if(impulseSource !=null)
+            impulseSource.GenerateImpulse();
         Debug.Log("Damage zone");
     }
 }
