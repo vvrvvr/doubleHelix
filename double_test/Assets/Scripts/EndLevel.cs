6b873088d9eb99810215e495061888f29cc2879d
v.svistunov <v.svistunov@corp.nekki.ru>
2023-11-04 20:03:31 +0300

    прототип конца уровня

---
+++ double_test/Assets/Scripts/EndLevel.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour, IInteractable
+{
+    public InteractionOption InteractionOption { get; set; }
+    public void Interact()
+    {
+        
+        GameManager.Instance.EndLevel();
+    } 
+}
