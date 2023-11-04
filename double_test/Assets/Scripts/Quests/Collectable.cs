f7a6f14bdf968f8e0f4098c98e573716d4ab2fb3
v.svistunov <v.svistunov@corp.nekki.ru>
2023-11-04 19:11:05 +0300

    коллектабл готов

--- double_test/Assets/Scripts/Quests/Collectable.cs
+++ double_test/Assets/Scripts/Quests/Collectable.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Collectable : Quest
 {
-    
+    [SerializeField] private GameObject effect;
     private void OnTriggerEnter(Collider other)
     {
         // Проверяем, если объект с тэгом "Player" входит в триггер
         if (other.CompareTag("Player" ))
         {
-           
+            Instantiate(effect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
             Debug.Log("trigger да");
             ConditionComplete();
 
