eeaa4008ac9d20e9c3f9fc4d2a7ea672d5484bda
v.svistunov <v.svistunov@corp.nekki.ru>
2023-11-05 16:57:34 +0300

    меню, ui, экран смерти

---
+++ double_test/Assets/Menu/Scripts/BackgroundGrid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundGrid : MonoBehaviour
+{
+    private Vector3 startPos;
+    [SerializeField] private float repeatWidth;
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < repeatWidth)
+        {
+            transform.position = startPos;
+        }
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+    }
+}
--- double_test/Assets/Scripts/GameManager.cs
+++ double_test/Assets/Scripts/GameManager.cs
@@ -15,9 +15,11 @@ public class GameManager : MonoBehaviour
     [Space(10)] [SerializeField] private int nextSceneNumber = 0;
     [Space(10)][SerializeField] private GameObject fadeIn;
     [SerializeField] private GameObject fadeOut;
+    [SerializeField] private GameObject DeathScreen;
     
     
     private static GameManager _instance;
+    public int maxLives = 3;
     public int lives = 3;
     public Vector3 currentCheckpoint;
     private CinemachineImpulseSource impulseSource;
@@ -50,11 +52,13 @@ public class GameManager : MonoBehaviour
         }
 
         fadeOut.SetActive(true);
+        DeathScreen.SetActive(false);
     }
     
     
     void Start()
     {
+        lives = maxLives;
         impulseSource = GetComponent<CinemachineImpulseSource>();
         livesText.text = "Lives: " + lives;
         currentCheckpoint = Vector3.zero;
@@ -101,11 +105,12 @@ public class GameManager : MonoBehaviour
         Debug.Log("death");
         PlayerController.Instance.TurnBombsOff();
         PlayerController.Instance.Death();
+        DeathScreen.SetActive(true);
     }
 
     public void RestartLevel()
     {
-        lives = 3;
+        lives = maxLives;
         livesText.text = "Lives: " + lives;
         currentCheckpoint = Vector3.zero;
         PlayerController.Instance.SpawnPlayer(StartPosition.transform.position);
@@ -142,9 +147,10 @@ public class GameManager : MonoBehaviour
 
     public void RestartLevelFromCheckpoint()
     {
+        DeathScreen.SetActive(false);
         if (currentCheckpoint != Vector3.zero)
         {
-            lives = 3;
+            lives = maxLives;
             livesText.text = "Lives: " + lives;
             PlayerController.Instance.SpawnPlayer(currentCheckpoint);
         }
